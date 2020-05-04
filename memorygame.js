bfaf569220e0ef366797cf5ef7e69b6777895d02
ahmedbfront <ahmedbfront@gmail.com>
2020-05-04 13:22:14 +0200

    update

--- memorygame.js
+++ memorygame.js
@@ -1,23 +1,3 @@
-// Select The Start Game Button
-document.querySelector(".control-buttons span").onclick = function () {
-  // Prompt Window To Ask For Name
-  let yourName = prompt("Whats Your Name?");
-
-  // If Name Is Empty
-  if (yourName == null || yourName == "") {
-    // Set Name To Unknown
-    document.querySelector(".name span").innerHTML = "Unknown";
-
-    // Name Is Not Empty
-  } else {
-    // Set Name To Your Name
-    document.querySelector(".name span").innerHTML = yourName;
-  }
-
-  // Remove Splash Screen
-  document.querySelector(".control-buttons").remove();
-};
-
 // Effect Duration
 let duration = 500;
 
